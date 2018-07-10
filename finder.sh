23208cef38e012a4b3da0093f98689466fde71ae
Henkkis <40563680+Henkkis@users.noreply.github.com>
2018-07-10 10:06:18 +0300

    Searching for specific libs

--- finder.sh
+++ finder.sh
@@ -1,12 +1,12 @@
 for dire in $(find $1 -name "lib"); do
 	echo -e "\n In directory $dire"
 	echo "--------------------------"
-	echo $(ldd $dire/* | grep plum)
+	echo $(ldd $dire/* | grep $2)
 	echo "--------------------------"
 done
 for dire in $(find $1 -name "lib64"); do
 	echo -e "\n In directory $dire"
 	echo "--------------------------"
-	echo $(ldd $dire/* | grep plum)
+	echo $(ldd $dire/* | grep $2)
 	echo "--------------------------"
 done
