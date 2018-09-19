1309856f09ee7ad4674eb925fc7dcd10c0edd3c7
Henkkis <40563680+Henkkis@users.noreply.github.com>
2018-09-19 18:36:39 +0300

    Moved stuff out of function

--- Random/mkl_numpy.py
+++ Random/mkl_numpy.py
@@ -3,30 +3,18 @@ import ctypes
 import numpy as np
 from numpy.ctypeslib import ndpointer
 
-N =10000000
-
 lib_c = ctypes.CDLL('./mkl_random.so')
-
-
-ct = ctypes.c_float*N
-arrp= ct()
-
-
 lib_c.c_randn_numpy.argtypes=[ctypes.c_int,ctypes.POINTER(ctypes.c_float)]
 lib_c.c_randn_numpy.restype = None
-
 lib_c.freeptr.argtype = ctypes.c_void_p
 lib_c.freeptr.restype = None
 
 
-lib_c.c_randn_numpy(N,arrp)
-a =np.ctypeslib.as_array(arrp,shape=(N,))
+def my_randn(N):
+    ct = ctypes.c_float*N
+    arrp= ct()
+    lib_c.c_randn_numpy(N,arrp)
+    return np.ctypeslib.as_array(arrp,shape=(N,))
 
 
-#for i in a:
-#    print(i)
 
-
-print(a[4])
-
-print("done")
