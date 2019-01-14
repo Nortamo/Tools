e1773de58bbbeb03e2e557503c6d11611b18b2fb
Henkkis <40563680+Henkkis@users.noreply.github.com>
2019-01-14 14:21:19 +0200

    Added better output

--- Small/finder.sh
+++ Small/finder.sh
@@ -1,14 +1,26 @@
 # search for $2 in all dynamic libraries
 # under $1 in directories named lib or lib64
 for dire in $(find $1 -name "lib"); do
-	echo -e "\n In directory $dire"
+	#echo -e "\n In directory $dire"
 	echo "--------------------------"
-	echo $(ldd $dire/*.so* | grep $2)
+	a=$(ldd $dire/*.so* | grep $2)
+	if [ -z "$a" ]
+	then
+      		echo "Dynamic library not found in $dire"
+	else
+      		echo "Dynamic library found in $dire"
+	fi		
 	echo "--------------------------"
 done
 for dire in $(find $1 -name "lib64"); do
-	echo -e "\n In directory $dire"
+	#echo -e "\n In directory $dire"
 	echo "--------------------------"
-	echo $(ldd $dire/*.so* | grep $2)
+	a=$(ldd $dire/*.so* | grep $2)
+	if [ -z "$a" ]
+	then
+      		echo "Dynamic library not found in $dire"
+	else
+      		echo "Dynamic library found in $dire"
+	fi		
 	echo "--------------------------"
 done
